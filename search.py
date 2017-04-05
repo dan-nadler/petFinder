426330de0e12602ede041f468f5ae84542a7c9a1
Dan Nadler <dnadler87@gmail.com>
2017-04-04 21:42:02 -0400

    added email and uploaded to AWS via zappa

--- search.py
+++ search.py
@@ -2,29 +2,80 @@ from datetime import datetime, timedelta
 import pytz
 from local_settings import *
 import petfinder
-import email
+from email.mime.text import MIMEText
+import smtplib
+from flask import Flask
+import logging
 
-api = petfinder.PetFinderClient(api_key=api_key, api_secret=api_secret)
 
-pets = []
-names = []
-for pet in api.shelter_getpets(id=shelter_id, status='A', output='full'):
-    if pet['lastUpdate'] > datetime.now().replace(tzinfo=pytz.timezone('America/New_York')) - timedelta(minutes=120):
-        if not pet['name'] in names:
-            names.append(pet['name'])
+def find_new_pets():
+    api = petfinder.PetFinderClient(api_key=api_key, api_secret=api_secret)
 
-            temp = {}
-            temp['name'] = pet['name']
-            temp['age'] = pet['age']
-            temp['breeds'] = pet['breeds']
-            temp['mix'] = pet['mix']
-            temp['size'] = pet['size']
-            temp['id'] = pet['id']
-            temp['photo'] = pet['photos'][0]['url']
+    pets = []
+    names = []
+    for pet in api.shelter_getpets(id=shelter_id, status='A', output='full'):
+        if pet['lastUpdate'] > datetime.now().replace(tzinfo=pytz.timezone('America/New_York')) - timedelta(minutes=15):
+            if not pet['name'] in names:
+                names.append(pet['name'])
 
-            pets.append(temp)
-            del temp
-        else:
-            break
+                temp = {}
+                temp['name'] = pet['name']
+                temp['age'] = pet['age']
+                temp['breeds'] = pet['breeds']
+                temp['mix'] = pet['mix']
+                temp['size'] = pet['size']
+                temp['id'] = pet['id']
+                temp['photo'] = pet['photos'][0]['url']
+                temp['updated'] = pet['lastUpdate']
 
-print(pets)
+                pets.append(temp)
+                del temp
+            else:
+                break
+    return pets
+
+
+def send_email(to, pets):
+    from_addr = EMAIL['USERNAME']
+    password = EMAIL['PASSWORD']
+
+    text = '<html><head></head><body>'
+    for pet in pets:
+        text += '<p>'
+        text += '<h1>%s</h1>' % pet['name']
+        text += '<img src=\'%s\'/>' % pet['photo']
+        text += '<ul>'
+        text += '<li>Listing updated: %s</li>' % pet['updated'].strftime('%m/%d/%Y %I:%M %p')
+        text += '<li>Age: %s</li>' % str(pet['age'])
+        text += '<li>Size: %s</li>' % pet['size']
+        text += '<li>Link: https://www.petfinder.com/petdetail/%s</li>' % pet['id']
+        text += '</ul>'
+        text += '</p>'
+    text += '</body></html>'
+
+    msg = MIMEText(text, 'html')
+    msg['Subject'] = 'petfinder Update!'
+    msg['From'] = from_addr
+    msg['To'] = to
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(from_addr, password)
+    server.sendmail(from_addr, to, msg.as_string())
+    server.quit()
+
+
+def check_pets():
+    logging.debug('Looking for new pets.')
+    pets = find_new_pets()
+    logging.debug('Found %i pets' % len(pets))
+    if len(pets) > 0:
+        logging.debug('Sending email')
+        send_email('gdaniels313@gmail.com,dnadler87@gmail.com', pets)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World. This is the petfinder finder!'
